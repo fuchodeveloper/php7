479683606c8c6cdea1605fd2bab618d5c2c19d36
fucho <fmgbeoma@yahoo.com>
2017-08-10 08:02:21 +0100

    working on tests

--- src/Domain/Sale.php
+++ src/Domain/Sale.php
@@ -4,9 +4,18 @@ namespace Bookstore\Domain;
 
 class Sale
 {
+    private $books = [];
 
     public function getBooks(): array
     {
-        return [];
+        return $this->books;
+    }
+
+    public function addBook(int $bookId, int $amount = 1)
+    {
+        if (!isset($this->books[$bookId])) {
+            $this->books[$bookId] = 0;
+        }
+        $this->books[$bookId] += $amount;
     }
 }
--- tests/Domain/SaleTest.php
+++ tests/Domain/SaleTest.php
@@ -18,8 +18,32 @@ class SaleTest extends TestCase
         $this->assertEmpty($sale->getBooks());
     }
 
-//    public function testSpecifyAmountBooks()
-//    {
-//
-//    }
+    public function testSpecifyAmountBooks()
+    {
+        $sale = new Sale();
+        $sale->addBook(123, 5);
+
+        $this->assertSame($sale->getBooks(), [123 => 5]);
+    }
+
+    public function testAddMultipleTimesSameBook()
+    {
+        $sale = new Sale();
+        $sale->addBook(123, 5);
+        $sale->addBook(123);
+        $sale->addBook(123, 5);
+
+        $this->assertSame($sale->getBooks(), [123 => 11]);
+    }
+
+    public function testAddDifferentBooks()
+    {
+        $sale = new Sale();
+        $sale->addBook(123, 5);
+        $sale->addBook(456, 2);
+        $sale->addBook(789, 5);
+
+        $this->assertSame($sale->getBooks(), [ 123 => 5, 456 => 2, 789 => 5 ]);
+    }
+
 }
